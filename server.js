7b4155ec4b3a5cf399adcff2b28066d043c3ed68
sandra-td <aleksandra.dahlin@gmail.com>
2020-05-08 13:59:59 +0200

    add user model and register route

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const connectToDb = require("./config/db");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+connectToDb();
+
+//Init middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) =>
     res.json({ msg: "Welcome to contact keeper's API" })
 );
-connectToDb();
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
