aed6dc4c6135b9dc345fa4f02085123df56d1f60
sandra-td <aleksandra.dahlin@gmail.com>
2020-05-17 18:03:57 +0200

    add basic ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
 const App = () => {
-    return <div className="App">my app</div>;
+    return (
+        <Router>
+            <React.Fragment>
+                <Navbar />
+                <div className="container">
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/about" component={About} />
+                    </Switch>
+                </div>
+            </React.Fragment>
+        </Router>
+    );
 };
 
 export default App;
